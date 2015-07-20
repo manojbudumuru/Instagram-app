351eb8c4ce082bfa87a68fce636232d36b4426e1
manoj <manoj.theknight@gmail.com>
2015-07-20 16:01:21 -0500

    Initial Commit

---
+++ photoTagApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  photoTagApp
+//
+//  Created by Saibersys on 7/20/15.
+//  Copyright (c) 2015 Saibersys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ photoTagApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  photoTagApp
+//
+//  Created by Saibersys on 7/20/15.
+//  Copyright (c) 2015 Saibersys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
